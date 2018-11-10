5cc05da17a0c302d80e979bd8cf840f0c940bc2e
HaHa21 <domlee2012@gmail.com>
2018-11-10 16:40:33 +0800

    Removed message module and message template. Completed service.

--- src/app/Mainpages/messages/message.service.ts
+++ src/app/Mainpages/messages/message.service.ts
@@ -1,96 +1,100 @@
-import { Http, Response, Headers } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable, EventEmitter } from "@angular/core";
-import 'rxjs/Rx';
-import { Observable } from "rxjs";
-
+import { Subject } from "rxjs";
+import { Router } from '@angular/router';
+import { map } from "rxjs/operators";
 import { Message } from "./message.model";
 import { ErrorService } from "../../Components/errors/error.service";
 
 @Injectable()
 export class MessageService {
-   private messages: Message[] = [];
-   messageIsEdit = new EventEmitter<Message>();
 
-   constructor( private errorService: ErrorService){
+  private posts: Message[] = [];
+  private postsUpdated = new Subject<{ posts: Message[]; postCount: number }>();
 
-   }
-
-   addMessage(message : Message){
-
-     const body = JSON.stringify(message);
-     const headers = new Headers({'Content-Type': 'application/json'});
-     const token = localStorage.getItem('token')
-            ? '?token=' + localStorage.getItem('token')
-            : '';
-        return this.http.post('http://localhost:3000/message' + token, body, {headers: headers})
-            .map((response: Response) => {
-                const result = response.json();
-                const message = new Message(
-                    result.obj.content,
-                    result.obj.user.firstName,
-                    result.obj._id,
-                    result.obj.user._id);
-                this.messages.push(message);
-                return message;
-            })
-            .catch((error: Response) => {
-                this.errorService.handleError();
-                return Observable.throw(error.json());
-            });
+   constructor(private http: HttpClient, private router : Router){
 
    }
 
-   getMessages() {
-     return this.http.get('http://localhost:3000/message')
-             .map((response: Response) => {
-                 const messages = response.json().obj;
-                 let transformedMessages: Message[] = [];
-                 for (let message of messages) {
-                     transformedMessages.push(new Message(
-                         message.content,
-                         message.user.firstName,
-                         message._id,
-                         message.user._id)
-                     );
-                 }
-                 this.messages = transformedMessages;
-                 return transformedMessages;
-             });
-            /*.catch((error: Response) => {
-                 this.errorService.handleError(error.json());
-                 return Observable.throw(error.json());*/
-
+   getMessages(postsPerPage: number, currentPage: number) {
+     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
+     this.http
+       .get<{ message: string; posts: any; maxPosts: number }>(
+         "http://localhost:3000/messages" + queryParams
+       )
+       .pipe(
+         map(postData => {
+           return {
+             posts: postData.posts.map(post => {
+               return {
+                 title: post.title,
+                 content: post.content,
+                    id: post._id,
+                  creator: post.creator
+               };
+             }),
+             maxPosts: postData.maxPosts
+           };
+         })
+       )
+       .subscribe(transformedPostData => {
+         this.posts = transformedPostData.posts;
+         this.postsUpdated.next({
+           posts: [...this.posts],
+           postCount: transformedPostData.maxPosts
+         });
+       });
    }
 
-   editMessage(message : Message){
-      this.messageIsEdit.emit(message);
+    updateMessage(id: string, title: string, content: string){
+        let messageData: Message | FormData;
+        messageData = new FormData();
+
+        messageData.append("id", id);
+        messageData.append("title", title);
+        messageData.append("content", content);
+
+        messageData = {
+        id: id,
+        title: title,
+        content: content,
+        creator: null
+      };
+
+      this.http
+      .put("http://localhost:3000/messages/" + id, messageData)
+      .subscribe(response => {
+        this.router.navigate(["/"]);
+      });
+    }
+
+
+     getMessageUpdateListener() {
+       return this.postsUpdated.asObservable();
+     }
+
+     getMessage(id: string) {
+       return this.http.get<{
+         _id: string;
+         title: string;
+         content: string;
+         creator: string;
+       }>("http://localhost:3000/messages/" + id);
+     }
+
+   addMessage(content : string, title: string, creator: string){
+       const message: Message = {id: null, title: title, content: content, creator: creator};
+       this.http.post<{ message: string; postId: string} >(
+         "http://localhost:3000/messages" , message
+       ).subscribe(responseData => {
+         this.router.navigate(["/"]);
+       })
+       console.log(content);
    }
 
-   updateMessage(message: Message){
-     const body = JSON.stringify(message);
-       const headers = new Headers({'Content-Type': 'application/json'});
-       const token = localStorage.getItem('token')
-           ? '?token=' + localStorage.getItem('token')
-           : '';
-       return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, {headers: headers})
-           .map((response: Response) => response.json())
-           .catch((error: Response) => {
-               this.errorService.handleError();
-               return Observable.throw(error.json());
-           });
-   }
+   deleteMessage(postId: string) {
+       return this.http.delete("http://localhost:3000/messages/" + postId);
+     }
 
-   deleteMessage( message : Message ){
-     this.messages.splice( this.messages.indexOf(message) , 1);
-     const token = localStorage.getItem('token')
-          ? '?token=' + localStorage.getItem('token')
-          : '';
-      return this.http.delete('http://localhost:3000/message/' + message.messageId + token)
-          .map((response: Response) => response.json())
-          .catch((error: Response) => {
-              this.errorService.handleError();
-              return Observable.throw(error.json());
-          });
-   }
 
 }
