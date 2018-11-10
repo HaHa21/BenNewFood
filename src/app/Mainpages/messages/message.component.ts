54a89aa232f19e4ac3e65cdcc73c5b9f977e31dd
HaHa21 <domlee2012@gmail.com>
2018-11-10 23:48:56 +0800

    Message Components Completed

--- src/app/Mainpages/messages/message.component.ts
+++ src/app/Mainpages/messages/message.component.ts
@@ -1,50 +1,96 @@
-import { Component, Input } from "@angular/core";
-
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Subscription } from "rxjs";
 import { Message } from "./message.model";
 import { MessageService } from "./message.service";
-
+import { AuthService } from "../auth/auth.service";
 
 @Component({
    selector: 'app-message-component',
-   templateUrl: './message.component.html',
-   styles: [`
-     .author {
-       display: inline-block;
-       font-style: italic;
-       font-size: 12px;
-       width: 80%;
-     }
+   templateUrl: './message.component.html'
 
-     .config{
-       display: inline-block;
-       text-align: right;
-       font-size: 12px;
-       width: 19%;
-     }
-
-     `]
 })
 
-export class MessageComponent {
-   @Input() message: Message;
+export class MessageComponent implements OnInit, OnDestroy{
+  enteredTitle = "";
+  enteredContent = "";
+  post: Message;
+  isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
+  private mode = "create";
+  private postId: string;
+  private authStatusSub: Subscription;
 
-
-   constructor(private messageService : MessageService){
+   constructor(public messageService : MessageService, public route : ActivatedRoute, private authService : AuthService){
 
    }
 
-   //Edit single message
-   onEdit() {
-     this.messageService.editMessage(this.message);
+   ngOnInit(){
+     this.authStatusSub = this.authService
+    .getAuthStatusListener()
+    .subscribe(authStatus => {
+      this.isLoading = false;
+    });
+     this.form = new FormGroup({
+       title: new FormControl(null, {
+  validators: [Validators.required, Validators.minLength(3)]
+}),
+           content: new FormControl(null, { validators: [Validators.required] })
+     })
+     this.route.paramMap.subscribe((paramMap: ParamMap) => {
+    if (paramMap.has("postId")) {
+      this.mode = "edit";
+      this.postId = paramMap.get("postId");
+      this.isLoading = true;
+      this.messageService.getMessage(this.postId).subscribe(postData => {
+        this.isLoading = false;
+        this.post = {
+
+          id: postData._id,
+          title: postData.title,
+          content: postData.content,
+          creator: postData.creator
+        };
+        this.form.setValue({
+          title: this.post.title,
+          content: this.post.content
+
+        });
+      });
+    } else {
+      this.mode = "create";
+      this.postId = null;
+    }
+  });
    }
 
-   onDelete() {
-     this.messageService.deleteMessage(this.message).subscribe(
-       result => console.log(result)
-     );
+   onSavePost() {
+     if (this.form.invalid) {
+       return;
+     }
+     this.isLoading = true;
+     if (this.mode === "create") {
+       console.log(this.form.value.content);
+       this.messageService.addMessage(
+         this.form.value.title,
+         this.form.value.content,
+         this.postId
+       );
+     } else {
+       this.messageService.updateMessage(
+      this.postId,
+
+      this.form.value.title,
+      this.form.value.content
+
+    );
+     }
+     this.form.reset();
    }
 
-   belongsToUser() {
-      return localStorage.getItem('userId') == this.message.userId;
-  }
+   ngOnDestroy() {
+     this.authStatusSub.unsubscribe();
+   }
 }
